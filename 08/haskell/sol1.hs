8ad638d5462fd88f8bc1c6c8cd3aa61dc04d91ad
Dylan Thinnes <dylan.thinnes@protonmail.com>
2023-12-14 17:44:33 +0000

    day 8 haskell

---
+++ 08/haskell/sol1.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE RecordWildCards #-}
+module Main where
+
+import qualified Data.Map as M
+
+main :: IO ()
+main = do
+  movements <- getLine
+  getLine
+  nodeMap <- M.fromList . map parseNode . lines <$> getContents
+  print movements
+  print nodeMap
+  let path = followPath nodeMap "AAA" (cycle movements)
+  print path
+  print (length path)
+
+data Node = Node { left, right :: String }
+  deriving (Show)
+
+parseNode :: String -> (String, Node)
+parseNode line =
+  let name = take 3 line
+      left = take 3 $ drop 7 line
+      right = take 3 $ drop 12 line
+  in
+  (name, Node{..})
+
+followPath :: M.Map String Node -> String -> String -> String
+followPath nodeMap "ZZZ" _ = ""
+followPath nodeMap node (dir:rest) =
+  let dirFunc = if dir == 'L' then left else right
+  in
+  dir : followPath nodeMap (dirFunc (nodeMap M.! node)) rest
